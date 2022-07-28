db4236001161b091400f9199ad5f66ef64d3b4b5
michaleona <patricia_jelcika@yahoo.com>
2022-07-28 09:00:48 +0000

    Deploying to gh-pages from @ michaleona/ajaib-test@70289e22764ac72c9017f80db4196e4e974e7f9c 🚀

---
+++ _next/static/qnv2Qgw0qGrY_HoB9e0Ti/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/704-70c6e5819a8144fa.js","static/chunks/pages/index-abe2262e6df551fa.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
