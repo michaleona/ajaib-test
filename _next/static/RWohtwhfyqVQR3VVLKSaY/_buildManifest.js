a57cb8998297312edd536e1e086137d691c6c892
michaleona <patricia_jelcika@yahoo.com>
2022-07-28 16:57:41 +0000

    Deploying to gh-pages from @ michaleona/ajaib-test@d50c5c249294179ce4f4bea47591c86bdc5e1a45 🚀

---
+++ _next/static/RWohtwhfyqVQR3VVLKSaY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-e61c2e0d9c9a0957.js","static/chunks/704-70c6e5819a8144fa.js","static/chunks/pages/index-abe13849427196d3.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/ZONP9KUGIEJ-9FY9DYHmn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-e61c2e0d9c9a0957.js","static/chunks/704-70c6e5819a8144fa.js","static/chunks/pages/index-903d347fde1b3558.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
