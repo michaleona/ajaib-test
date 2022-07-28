11198d1b87fe0f44b068d5e5680b0e03137c2cc4
michaleona <patricia_jelcika@yahoo.com>
2022-07-28 08:36:55 +0000

    Deploying to gh-pages from @ michaleona/ajaib-test@ae89b0834fb6743e31bb0f5cbe7fc65b9ffa8d5b 🚀

--- _next/static/3sv3woXmb01aWFyp9Jo-Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/699-a21800cc2626658d.js","static/chunks/pages/index-4490cd7bbba14fb6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
