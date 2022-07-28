0f19958c25922d2b1bd189c2b20ffc9083180e9e
Patricia <patricia@prixa.ai>
2022-07-28 10:11:09 +0700

    Add env variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  basePath: "/ajaib-test",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import Head from "next/head";
 import "antd/dist/antd.css";
 
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 export default ({ Component, pageProps }) => {
   return (
     <Fragment>
       <Head>
         <title>Ajaib Test</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${prefix}/favicon.ico`} />
       </Head>
       <main>
         <Component {...pageProps} />
