1d0dbfea1e642d014d92201010280dfd60b69237
michaleona <patricia_jelcika@yahoo.com>
2022-07-28 09:37:34 +0000

    Deploying to gh-pages from @ michaleona/ajaib-test@a034e6e8ad300fafb2e9b02b3366194551708273 🚀

---
+++ _next/static/lUgJEQyNWKipp5GD7hdkf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/704-70c6e5819a8144fa.js","static/chunks/pages/index-38b2442640e12df9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
